45b8aa000c8c75a426ddaaa290c39f6b1dc72ed4
Leonardo Boulitreau <l218479@dac.unicamp.br>
2023-01-30 23:03:00 -0300

    typo

--- TTS/style_encoder/configs/style_encoder_config.py
+++ TTS/style_encoder/configs/style_encoder_config.py
@@ -92,7 +92,7 @@ class StyleEncoderConfig(Coqpit):
         """Check config fields"""
         c = asdict(self)
         super().check_values()
-        check_argument("se_type", c, restricted=True, enum_list=["gst", "re","vae","vqvae" "diffusion", "vaeflow"])
+        check_argument("se_type", c, restricted=True, enum_list=["gst", "re","vae","vqvae", "diffusion", "vaeflow"])
         check_argument("agg_type", c, restricted=True, enum_list=["sum", "concat", "adain"])
         check_argument("num_mel", c, restricted=False)
         check_argument("style_embedding_dim", c, restricted=True, min_val=0, max_val=1000)
