072ddbcbf8d188820c78b4730479af297b21fe80
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-02-03 18:58:12 -0300

    print debug

--- TTS/tts/models/styleforward_tts.py
+++ TTS/tts/models/styleforward_tts.py
@@ -736,8 +736,8 @@ class StyleforwardTTS(BaseTTS):
             style_encoder_outputs = self.style_encoder_layer.inference(o_en.permute(0,2,1), style_mel = aux_input['style_mel'], z = aux_input['z'])
             o_en = style_encoder_outputs['styled_inputs'].permute(0,2,1)
         else:
-            se_inputs = [o_en.permute(0,2,1), aux_input['style_mel']]
-            style_encoder_outputs = self.style_encoder_layer.inference(se_inputs)
+            # se_inputs = [o_en.permute(0,2,1), aux_input['style_mel']]
+            style_encoder_outputs = self.style_encoder_layer.inference(o_en.permute(0,2,1), style_mel = aux_input['style_mel'])
             o_en = style_encoder_outputs['styled_inputs'].permute(0,2,1)
 
 
