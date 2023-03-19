1e0cc2e8a79361324251d29bd5e75a59d34a3004
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-03-18 22:37:11 -0300

    print debug

--- TTS/tts/layers/losses.py
+++ TTS/tts/layers/losses.py
@@ -1171,11 +1171,11 @@ class StyleForwardTTSLoss(nn.Module):
 
         if self.style_encoder_config.use_grl_on_speakers_in_style_embedding:
             print(speaker_preds_from_style.shape, speaker_ids.shape)
-            grl_speaker_in_style_loss = self.criterion_grl_speaker_in_style_embedding(speaker_preds_from_style, speaker_ids)
+            # grl_speaker_in_style_loss = self.criterion_grl_speaker_in_style_embedding(speaker_preds_from_style, speaker_ids)
             
-            loss += self.grl_speaker_in_style_embedding_alpha*grl_speaker_in_style_loss
+            # loss += self.grl_speaker_in_style_embedding_alpha*grl_speaker_in_style_loss
             
-            return_dict["grl_speaker_in_style_embedding"] = grl_speaker_in_style_loss
+            # return_dict["grl_speaker_in_style_embedding"] = grl_speaker_in_style_loss
 
 
         if(self.style_encoder_config.use_clip_loss):
