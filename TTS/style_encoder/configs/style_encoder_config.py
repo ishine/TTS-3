ed19890ebe36f4af7cb7be49151a6d0d5268d58c
Leonardo Boulitreau <l218479@dac.unicamp.br>
2023-04-18 23:01:48 -0300

    insert loss types on config, remove grl use_balanced!

--- TTS/style_encoder/configs/style_encoder_config.py
+++ TTS/style_encoder/configs/style_encoder_config.py
@@ -21,12 +21,17 @@ class StyleEncoderConfig(Coqpit):
         se_type (str):
             Style encoder type \in {diffusion, vae, vaeflow, gst, re}
     """
+    
+    #########################
+    #### General Configs ####
+    #########################
+    
     # Style Encoder Type
     se_type: str = "re"
-    use_supervised_style: bool = False
-    use_guided_style: bool = False # Whether use guided style encoder training
-    use_lookup: bool = False
-
+    start_loss_at: int = 0 # Currently only used on NON-RE-GST architectures
+    use_supervised_style: bool = False # Enables supervision (Look-Up or style class)
+    use_lookup: bool = False 
+        
     # Style Encoder Inputs
     num_mel: int = 80
     decompose_ref_mel: bool = False
@@ -41,20 +46,30 @@ class StyleEncoderConfig(Coqpit):
     agg_norm: bool = False # If agg_type == sum, you can rather than normalizing or not
     use_proj_linear: bool = False # Whether use linear projection to decoder dim or not (specifcally useful for sum agg_style)
 
-    # Generic Style Encoder Configuration
-    start_loss_at: int = 0 # Iteration that the style loss should start propagate 
+    # Style Classifier
+    use_guided_style: bool = False # Whether use guided style encoder training
+    guided_style_loss_type: str = 'ce' # or focal_loss
+    
+    # Speaker Classifier + GRL
+    use_grl_on_speakers_in_style_embedding: bool = False # Whether use or not GRL in style embedding output avoinding speaker information
+    grl_loss_type: str = 'ce' # or focal loss
+    use_inverter: bool = False # whether use gri instead of grl
+    grl_alpha: float = 1 # GRL alpha, still one for all GRL's
+        
+    # Orthogonal losses   
+    orthogonal_loss: bool = False  
+    orthogonal_loss_alpha: float = 1.0
+    content_orthogonal_loss: bool = False # whether use othogonal loss between style and content embeddings
+    speaker_orthogonal_loss: bool = False # whether use othogonal loss between speaker and content embeddings
+        
+    # Residual Speaker
     use_residual_speaker_disentanglement: bool = False # If true, decomposes style embedding in F and SE-F, where SE-F will represent speakers
 
-    # Finegrained re specific configs
-    fg_attention_dropout: float = 0.1
-    fg_attention_dim: int = 384
-    prosody_embedding_dim: int = 3 #Default to align with text, according to reference repo, by my first look it should be = proj_dim = encoder_output_dim
-
-    # Cycle consistency configs
+    # Cycle consistency
     use_cycle_consistency: bool = False 
     cycle_consistency_alpha: float = 1
     start_cycle_at: int = 1 # Step to start back propagating cycle loss
-
+        
     # CLIP loss configs
     use_clip_loss: bool = False 
     clip_alpha_loss: float = 1
@@ -63,36 +78,39 @@ class StyleEncoderConfig(Coqpit):
     # Style ditortion configs
     use_style_distortion_loss: bool = False
     style_distortion_alpha_loss: float = 1
+        
+    #######################################
+    #### Architecture Specific Configs ####
+    #######################################
+    
+    # Finegrained RE 
+    fg_attention_dropout: float = 0.1
+    fg_attention_dim: int = 384
+    prosody_embedding_dim: int = 3 #Default to align with text, according to reference repo, by my first look it should be = proj_dim = encoder_output_dim
 
-    # GRL additional configs
-    use_grl_on_speakers_in_style_embedding: bool = False # Whether use or not GRL in style embedding output avoinding speaker information
-    balanced_grl: bool = False # whether use focal loss or not
-    use_inverter: bool = False # whether use gri instead of grl
-    grl_alpha: float = 1 # GRL alpha, still one for all GRL's
-
-    # GST-SE Additional Configs
+    # GST
     gst_style_input_weights: dict = None
     gst_num_heads: int = 4
     gst_num_style_tokens: int = 10
 
-    # VAE-Based General Configs
+    # VAE
     use_cyclical_annealing: bool = True # Whether use or not annealing (recommended true), only linear implemented
     vae_loss_alpha: float = 1.0 # Default alpha value (term of KL loss)
     vae_cycle_period: int = 5000 # iteration period to apply a new annealing cycle
 
-    # VAE-Based Additional Configs
+    # VQVAE
     codebook_size: int = 16 # Codebook size
     vqvae_commitment_beta: float = 0.25 # Depends on the scale of the reconstruction loss
 
-    # VAEFLOW-SE Additional Configs
+    # VAEFLOW
     vaeflow_intern_dim: int = 300
     vaeflow_number_of_flows: int = 16
 
-    # Diffusion-SE Additional Configs
+    # Diffusion
     diff_num_timesteps: int = 25 
     diff_schedule_type: str = 'cosine'
     diff_loss_type: str = 'l1' 
-    diff_ref_online: bool = True 
+    diff_ref_online: bool = True
     diff_step_dim: int = 128
     diff_in_out_ch: int = 1 
     diff_num_heads: int = 1 
@@ -100,12 +118,6 @@ class StyleEncoderConfig(Coqpit):
     diff_num_blocks: int = 5
     diff_dropout: float = 0.1
     diff_loss_alpha: float = 0.75
-
-    # Use orthogonal loss    
-    orthogonal_loss: bool = False  
-    orthogonal_loss_alpha: float = 1.0
-    content_orthogonal_loss: bool = False # whether use othogonal loss between style and content embeddings
-    speaker_orthogonal_loss: bool = False # whether use othogonal loss between speaker and content embeddings
 
     def check_values(
         self,
