d235cdc71891824f81fe3677a1490b824376611c
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-08-15 15:30:12 -0300

    print debug

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -117,6 +117,7 @@ def ljspeech(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
     with open(txt_file, "r", encoding="utf-8") as ttf:
         for line in ttf:
             cols = line.split("|")
+            print(cols)
             wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
             text = cols[2]
             items.append([text, wav_file, speaker_name])
