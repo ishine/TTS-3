d0e25914d0dee61d834d124d29c4d8159a5270b0
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-08-21 10:07:38 -0300

    print module

--- TTS/utils/generic_utils.py
+++ TTS/utils/generic_utils.py
@@ -92,7 +92,9 @@ def find_module(module_path: str, module_name: str) -> object:
     module_name = module_name.lower()
     print(module_name)
     module = importlib.import_module(module_path + "." + module_name)
+    print(module)
     class_name = to_camel(module_name)
+    print(class_name)
     return getattr(module, class_name)
 
 
